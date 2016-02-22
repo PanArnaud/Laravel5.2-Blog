c98f47e197ea33fd09c2bb8c157173d88b61a923
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2016-02-22 09:48:46 +0400

    Basic laravel
    0 edition

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+    </body>
+</html>
+vb

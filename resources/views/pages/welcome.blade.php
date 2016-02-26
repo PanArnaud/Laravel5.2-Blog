1472d15420715615b6cb2a4cabe80218bacc4b42
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2016-02-26 09:21:49 +0400

    Full partial views

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('main')
 
+@section('title', 'Homepage')
+
 @section('content')
   <div class="row">
     <div class="col-md-12">
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<hr>
+
+<p class="text-center">
+	Copyright Arnaud Panapadéatchy - All Rights Reserved
+</p>

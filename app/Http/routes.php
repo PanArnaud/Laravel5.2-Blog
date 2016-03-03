6764c627efb2f77f03fd5a6886d5eaec02399ec2
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2016-03-03 11:19:33 +0400

    Add somes button and information on the post view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,6 @@
 |
 */
 
-Route::get('contact', 'PagesController@getContact');
-Route::get('about', 'PagesController@getAbout');
-Route::get('/', 'PagesController@getIndex');
-Route::resource('posts', 'PostController');
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes

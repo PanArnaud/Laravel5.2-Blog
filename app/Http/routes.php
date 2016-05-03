ac6acaff48a5dfc8fc9ca86b277309c34d1abfd9
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2016-05-03 14:44:02 +0400

    Add view for the categories, and a form to add categories
    CategoeriesController made with make:controller --resource

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,7 +40,10 @@ Route::group(['middleware' => ['web']], function () {
 	Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
 	Route::post('password/reset', 'Auth\PasswordController@reset');
 
-		// Other Pages
+	// Categories routes
+	Route::resource('categories', 'CategoryController', ['except' => ['create']]);
+
+	// Other Pages
 	Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 	Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
 	Route::get('contact', 'PagesController@getContact');

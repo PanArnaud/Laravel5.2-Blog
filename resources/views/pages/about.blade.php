775111fe4fbe4ec14d0e451bcc451ce49e765e2f
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2016-07-28 14:53:07 +0400

    About page
    Add TimelineJs

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,8 +5,36 @@
 @section('content')
   <div class="row">
     <div class="col-md-12">
-      <h3>AA propos de moi</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic dicta quis ea ipsum veniam sequi, recusandae modi harum? Ullam mollitia optio in saepe quam facere minima repellendus, aspernatur cum accusantium.</p>
+      	<h3><i class="fa fa-user" aria-hidden="true"></i>  A propos de moi</h3>
+      	<div class="clearfix">
+  			<img class="pull-right img-responsive img-rounded a-propos-photo" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAI-AAAAJDQ1MDE5MjNhLTA5ZDAtNDkxYi04NDVmLWJmNjUwMWIwZGRjYw.jpg" alt="">
+  			<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, nobis velit asperiores autem. Repudiandae debitis maxime ratione esse enim consequuntur in nesciunt reiciendis recusandae a, natus cum ut architecto corporis dolor nisi earum iusto harum rem, ullam voluptatibus! Perferendis, velit commodi nam quis sapiente iusto tempora at aliquam vero voluptates, numquam aspernatur blanditiis voluptas? Atque ullam sunt asperiores, accusamus. Eaque repellat delectus assumenda velit, autem ipsa voluptate aliquam a accusamus ad nostrum, quisquam doloremque culpa unde nesciunt et magni nemo veritatis ipsam corrupti hic. Totam, odio sapiente ullam dolor mollitia minima eos facilis culpa ipsam iste nulla aspernatur consequuntur pariatur placeat doloribus reiciendis eaque itaque perferendis beatae. Omnis in provident minima illum culpa corporis nam quam. Ipsum provident, esse. Laborum adipisci quaerat, voluptatibus assumenda. Magni soluta amet vel labore nemo! Quisquam rerum, nam saepe! Quos maiores, neque architecto a sint consectetur amet animi ea nisi. Veritatis fugit, mollitia ex error sit, iure eum corporis voluptatibus, quod earum similique. Quisquam quaerat, vel voluptatibus, tenetur aliquam debitis aspernatur. Animi eaque nisi at delectus repellat itaque id ex ullam blanditiis deserunt. Repellendus fuga temporibus dolores illum quisquam, aliquid, dolorum praesentium voluptatibus rerum autem quos at beatae fugit, cupiditate ipsam quas, reiciendis vitae assumenda molestiae magnam excepturi animi. Doloremque eaque magnam, eveniet placeat qui incidunt quibusdam itaque ipsum, repellendus velit optio aut harum blanditiis hic voluptates ipsa unde modi. Soluta libero sed, voluptates suscipit at mollitia dolorem voluptate saepe velit qui ea in animi. Illo doloribus magnam repudiandae aut maxime sit temporibus natus illum modi eaque.</p>
+		</div>
+		<hr>
+
+		<h4 id="experiences"><i class="fa fa-angle-double-left" aria-hidden="true"></i>  Expériences</h4>
+		<iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1umu-9kNUNnPzDzjWGe_8XNK7bYlbGj0dC0atUHJzg_Y&font=Default&lang=fr&start_at_end=true&timenav_position=top&initial_zoom=1&height=500' width='100%' height='500' frameborder='0'></iframe>
+		<hr>
+
+		<h4 id="projets"><i class="fa fa-cogs" aria-hidden="true"></i>   Projets</h4>
+		<hr>
+
+		<h4><i class="fa fa-question-circle-o" aria-hidden="true"></i>    Centres d'intérêts</h4>
+		<div id="hobbies" class="row hobbies">
+        	<div class="col-lg-4">
+          		<i class="fa fa-gamepad fa-2x" aria-hidden="true"></i>
+      			<p>Jeux Vidéos</p>
+        	</div><!-- /.col-lg-4 -->
+        	<div class="col-lg-4">
+          		<i class="fa fa-music fa-2x" aria-hidden="true"></i>
+      			<p>Musique (Piano, guitare, ...)</p>
+        	</div><!-- /.col-lg-4 -->
+        	<div class="col-lg-4">
+          		<i class="fa fa-television fa-2x" aria-hidden="true"></i>
+      			<p>Séries (Game Of Thrones, Vikings, The Walking Dead, Shameless US, ...)</p>
+        	</div><!-- /.col-lg-4 -->
+      	</div><!-- /.row -->
     </div>
   </div>
+
 @endsection
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,6 +2,8 @@
 
 <p class="text-center">
 	ar-pa 2016 <br>
-	<a href="https://github.com/PanArnaud"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>     
-	<a href="https://www.linkedin.com/pub/arnaud-panapadéatchy/96/829/60a"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+	<span>
+		<a class="text-primary" href="https://github.com/PanArnaud"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>     
+	<a class="text-primary" href="https://www.linkedin.com/pub/arnaud-panapadéatchy/96/829/60a"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+	</span>
 </p> 	

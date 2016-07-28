728ca16d3894d4dfac0e67a6b67d5b47c6679052
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2016-07-28 15:02:21 +0400

    Edit photo on the welcome page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -15,7 +15,7 @@
       @endforeach
     </div>
     <div class="col-md-3 col-md-offset-1">
-      <img src="{{ 'https://www.gravatar.com/avatar/'.md5(strtolower(trim('  a.panapadeatchy@hotmail.fr'))).'?s=100' }}" alt="..." class="img-responsive center-block img-circle"> 
+      <img src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAI-AAAAJDQ1MDE5MjNhLTA5ZDAtNDkxYi04NDVmLWJmNjUwMWIwZGRjYw.jpg" alt="..." class="img-responsive center-block img-circle" width="150px" height="150px"> 
       <h3 class="text-center">Arnaud Panapadéatchy <small>Développeur web autodidacte</small></h3>
       <a href="/about" class="btn btn-xs btn-default center-block">En savoir plus
       </a>
